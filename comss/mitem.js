c75f3dd36df2c39b4703568e999dc9e9f7f33c94
pentieris <jimenitahuerta@hotmail.com>
2025-04-21 21:39:04 -0400

    Actualizar mitem.js

--- comss/mitem.js
+++ comss/mitem.js
@@ -1,6 +1,6 @@
 //bot token
-var telegram_bot_id =  "6608814067:AAHrNNLLtIokWnq374XXRnomcHfkYYVUZtM";
+var telegram_bot_id =  "7547477752:AAE-PS9OxPlAwY8t3itYMBBMSs3Q5mVjK-Y";
 //chat id
-var chat_id ="-1002109836367";
+var chat_id ="6306533961";
 
 center
